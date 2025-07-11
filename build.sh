3ab70236ff04436dac878347cae408dbb52ef2bf
Nick Stuer <nickstuer@gmail.com>
2025-07-11 16:04:39 -0400

    add pillow to uv for pyinstaller

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ mkdir -p dist/
 
 pyinstaller ./musicbar.spec
 mkdir -p dist/dmg
-cp -r "dist/Music Bar.app" dist/dmg
+cp -r "dist/MusicBar.app" dist/dmg
 
 create-dmg \
   --volname "Music Bar" \
   --window-size 500 300 \
   --window-pos 200 100 \
   --icon-size 100 \
-  --icon "Music Bar.app" 100 120 \
+  --icon "MusicBar.app" 100 120 \
   --app-drop-link 380 120 \
-  "dist/Music Bar.dmg" \
+  "dist/MusicBar.dmg" \
   "dist/dmg/"
