2f1618b1cca9760c64d8362403015710a8d0df5e
Nick Stuer <nickstuer@gmail.com>
2025-07-10 21:56:01 -0400

    change log format

--- src/music_bar.py
+++ src/music_bar.py
@@ -19,7 +19,7 @@ os.makedirs(log_directory, exist_ok=True)
 
 handler = logging.FileHandler(log_directory / "app.log")
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
